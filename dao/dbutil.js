32371a169b198cf8fb2e3dd0d764d0240a90db61
rebeccaWang <13717184357@163.com>
2019-09-25 14:22:40 +0800

    aliyun

--- dao/dbutil.js
+++ dao/dbutil.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const createConnection = () =>{
     return mysql.createConnection({
-        host: '192.168.1.8',
+        host: '120.77.46.101',
         port: '3306',
         user: 'root',
-        password: 'rebecca2010650403',
+        password: 'root',
         database:'my_blog'
     })
 }
